77bd0a67c241b5594135d2f2bd240e5939043065
MacOS <macos@MacOSs-MacBook-Pro.local>
2019-03-21 03:22:52 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  let scope = [];
+
+  for (let i= 0; i < str.length; i++) {
+    for (let x = 0; x < config.length; x++) {
+      if(config[x][0] === str[i])  {
+        if(str[i] === scope[scope.length - 1] && config[x][0] === config[x][1]) {
+          scope.pop();
+          break;
+        }
+          scope.push(str[i]);
+          break;
+      }
+      if(config[x][1] === str[i]){
+        if(config[x][0] === scope[scope.length - 1]) {        
+          scope.pop()
+          break;
+        } else return false;
+      }
+    }
+  }
+  return scope.length === 0;
 }
